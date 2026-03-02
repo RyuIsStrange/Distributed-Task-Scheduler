b0f6cb80477d9d690181746337341b5fb713c36f
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2026-03-02 14:46:03 -0600

    refactor(Coord): made fnc to fetch things from DB
    removes the excess/duplicated code that was there previously. there are issues with this but I will come back to this later

--- crates/coordinator/src/db.rs
+++ crates/coordinator/src/db.rs
@@ -5,8 +5,7 @@ use common::job::{
         Job,
 };
 use rusqlite::{
-    Connection, 
-    Error
+    Connection, Error, params
 };
 use std::str::FromStr;
 use chrono::{DateTime, Utc};
@@ -111,15 +110,30 @@ pub fn update_retry_count(conn: &Connection, job_id: Uuid, count: u32) -> Result
     Ok(())
 }
 
-pub fn load_pending_jobs(conn: &Connection) -> Result<Vec<Job>, Error> {
-    let mut stmt = conn.prepare(
-        "SELECT id, command, args, status, timestamp, retry_count, max_retries, priority, schedule, is_recurring, next_run, parent_schedule_id
-        FROM jobs 
-        WHERE status IN ('PENDING', 'RUNNING')
-        ORDER BY timestamp ASC"
-    )?;
+pub fn fetch_from_db(conn: &Connection, status: Option<JobStatus>) -> Result<Vec<Job>, Error> {
+    // let param ;
+    // if status.is_some() {
+    //     param = params![status.unwrap().to_string()];
+    // } else {
+    //     param = params![];
+    // }
 
-    let jobs = stmt.query_map([], |row| {
+    let (mut stmt, param) = if let Some(s) = status {
+        (conn.prepare(
+            "SELECT id, command, args, status, timestamp, retry_count, max_retries, priority, schedule, is_recurring, next_run, parent_schedule_id
+            FROM jobs 
+            WHERE status IN (?1)
+            ORDER BY timestamp ASC"
+        )?, params![s.to_string()])
+    } else {
+        (conn.prepare(
+            "SELECT id, command, args, status, timestamp, retry_count, max_retries, priority, schedule, is_recurring, next_run, parent_schedule_id
+            FROM jobs 
+            ORDER BY timestamp ASC"
+        )?, params![])
+    };
+
+    let jobs = stmt.query_map(param, |row| {
         let id_str: String = row.get(0)?;
         let command: String = row.get(1)?;
         let args_str: String = row.get(2)?;
@@ -136,7 +150,9 @@ pub fn load_pending_jobs(conn: &Connection) -> Result<Vec<Job>, Error> {
         let next_run_str: Option<String> = Some(row.get(10)?);
         let parent_id: Option<String> = row.get(11)?;
         
-        
+        // TODO: Better error handling in unwraps (ex: id: Uuid::from_str(...).map_err(|_| Error::InvalidColumnType(...
+        // Or Leave it, however it would be nice incase if something corrupts or becomes malformed 
+
         let (schedule, is_recurring, next_run, p_id) = if schedule.as_deref() == Some("None") {
             (None, false, None, None)
         } else {
@@ -168,132 +184,30 @@ pub fn load_pending_jobs(conn: &Connection) -> Result<Vec<Job>, Error> {
     })?;
     
     let result: Result<Vec<Job>, _> = jobs.collect();
+
+    result
+}
+
+pub fn load_pending_jobs(conn: &Connection) -> Result<Vec<Job>, Error> {
+    let pending = fetch_from_db(conn, Some(JobStatus::PENDING));
+    let running = fetch_from_db(conn, Some(JobStatus::RUNNING));
     
-    result  
+    let result: Result<Vec<Job>, Error> ;
+
+    if pending.is_ok() && running.is_ok() {
+        result = Ok([pending.unwrap(), running.unwrap()].concat());
+    } else if pending.is_err() && running.is_ok() {
+        result = running;
+    } else {
+        result = pending;
+    }
+
+    
+    result   
 }
 
 pub fn get_job_list(conn: &Connection, status: Option<JobStatus>) -> Result<Vec<Job>, Error> {
-    let mut stmt;
+    let result = fetch_from_db(conn, status);
 
-    match status {
-        Some(status) => {
-            stmt = conn.prepare(
-                "SELECT id, command, args, status, timestamp, retry_count, max_retries, priority, schedule, is_recurring, next_run, parent_schedule_id
-                FROM jobs 
-                WHERE status IN (?1)
-                ORDER BY timestamp ASC"
-            )?;
-
-            let jobs = stmt.query_map([status.to_string()], |row| {
-                let id_str: String = row.get(0)?;
-                let command: String = row.get(1)?;
-                let args_str: String = row.get(2)?;
-                let status_str: String = row.get(3)?;
-                let timestamp_str: String = row.get(4)?;
-
-                let retry_cnt: u32 = row.get(5)?;
-                let max_retry_cnt: u32 = row.get(6)?;
-
-                let priority: String = row.get(7)?;
-
-                let schedule: Option<String> = Some(row.get(8)?);
-                let is_recurring: bool = row.get(9)?;
-                let next_run_str: Option<String> = Some(row.get(10)?);
-                let parent_id: Option<String> = row.get(11)?;
-                
-                
-                let (schedule, is_recurring, next_run, p_id) = if schedule.as_deref() == Some("None") {
-                    (None, false, None, None)
-                } else {
-                    (
-                        schedule,
-                        is_recurring,
-                        next_run_str.and_then(|s| DateTime::parse_from_rfc3339(&s).ok().map(|dt| dt.into())),
-                        parent_id.and_then(|s| Uuid::from_str(&s).ok())
-                    )
-                };
-
-                Ok(Job { 
-                    id: Uuid::from_str(&id_str).unwrap(), 
-                    command, 
-                    args: serde_json::from_str::<Vec<String>>(&args_str).unwrap(), 
-                    status: JobStatus::from_str(&status_str).unwrap(), 
-                    timestamp: DateTime::parse_from_rfc3339(&timestamp_str).unwrap().into(),
-                    
-                    retry_count: retry_cnt,
-                    max_retries: max_retry_cnt,
-
-                    priority: Priority::from_str(&priority).unwrap(),
-
-                    schedule,
-                    is_recurring: Some(is_recurring),
-                    next_run,
-                    parent_schedule_id: p_id
-                })
-            })?;
-            
-            let result: Result<Vec<Job>, _> = jobs.collect();
-
-            return result
-        }
-        None => {
-            stmt = conn.prepare(
-                "SELECT id, command, args, status, timestamp, retry_count, max_retries, priority, schedule, is_recurring, next_run, parent_schedule_id
-                FROM jobs 
-                ORDER BY timestamp ASC"
-            )?;
-
-            let jobs = stmt.query_map([], |row| {
-                let id_str: String = row.get(0)?;
-                let command: String = row.get(1)?;
-                let args_str: String = row.get(2)?;
-                let status_str: String = row.get(3)?;
-                let timestamp_str: String = row.get(4)?;
-
-                let retry_cnt: u32 = row.get(5)?;
-                let max_retry_cnt: u32 = row.get(6)?;
-
-                let priority: String = row.get(7)?;
-
-                let schedule: Option<String> = Some(row.get(8)?);
-                let is_recurring: bool = row.get(9)?;
-                let next_run_str: Option<String> = Some(row.get(10)?);
-                let parent_id: Option<String> = row.get(11)?;
-                
-                
-                let (schedule, is_recurring, next_run, p_id) = if schedule.as_deref() == Some("None") {
-                    (None, false, None, None)
-                } else {
-                    (
-                        schedule,
-                        is_recurring,
-                        next_run_str.and_then(|s| DateTime::parse_from_rfc3339(&s).ok().map(|dt| dt.into())),
-                        parent_id.and_then(|s| Uuid::from_str(&s).ok())
-                    )
-                };
-
-                Ok(Job { 
-                    id: Uuid::from_str(&id_str).unwrap(), 
-                    command, 
-                    args: serde_json::from_str::<Vec<String>>(&args_str).unwrap(), 
-                    status: JobStatus::from_str(&status_str).unwrap(), 
-                    timestamp: DateTime::parse_from_rfc3339(&timestamp_str).unwrap().into(),
-                    
-                    retry_count: retry_cnt,
-                    max_retries: max_retry_cnt,
-
-                    priority: Priority::from_str(&priority).unwrap(),
-
-                    schedule,
-                    is_recurring: Some(is_recurring),
-                    next_run,
-                    parent_schedule_id: p_id
-                })
-            })?;
-            
-            let result: Result<Vec<Job>, _> = jobs.collect();
-
-            return result
-        }
-    }
+    result
 }
