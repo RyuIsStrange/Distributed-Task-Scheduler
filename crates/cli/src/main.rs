afc9833f145fbe059c2d3e977721fa2b97ab73cd
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2026-03-05 08:21:40 -0600

    chore(CLI)/wip: input descriptions/ job dependency system
    Added descriptions to the commands so when running --help you give more info for inputs
    
    Put the argument for dependency into the command line

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use uuid::Uuid;
 
 use crate::commands::{list, status, submit};
 
@@ -16,14 +17,21 @@ struct Cli {
 enum Commands {
     /// Submit new job
     Submit {
+        /// Command to run
         #[arg()]
         command: String,
+        /// Arguments for the command
         #[arg(long)]
         args: Option<String>,
+        /// Priority of the job. Options: High, Medium, or Low
         #[arg(long)]
         priority: Option<String>,
+        /// 5-6 Length cron schedule
         #[arg(long)]
-        schedule: Option<String>
+        schedule: Option<String>,
+        /// UUID of job required to finish for this one to run
+        #[arg(long)]
+        dependant: Option<Uuid>
     },
     
     /// Check job status
@@ -34,6 +42,7 @@ enum Commands {
     
     /// List jobs
     List {
+        /// Status to filter jobs by
         #[arg(long)]
         status: Option<String>,
     },
@@ -46,8 +55,18 @@ async fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Submit { command , args, priority, schedule} => { submit::job(command, args, priority, schedule).await; },
+        Commands::Submit { command , args, priority, schedule, dependant} => { 
+            submit::job(
+                command, 
+                args, 
+                priority, 
+                schedule,
+                dependant
+            ).await; 
+        },
+
         Commands::Status { job_id } => { status::fetch(job_id).await; },
+
         Commands::List { status } => { list::jobs(status).await; }
     }
 }
