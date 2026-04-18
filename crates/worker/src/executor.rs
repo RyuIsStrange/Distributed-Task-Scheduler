6e3807602bc6f03fc78295c65f284f0362ec2fbe
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2026-04-17 20:45:25 -0500

    feat: error handling if command output fails
    If the command fails, such as the command not existing or not having permission. There is now correct error handling for it

--- crates/worker/src/executor.rs
+++ crates/worker/src/executor.rs
@@ -4,6 +4,8 @@ use common::job::{
 };
 use tokio::process::Command;
 
+const FAILED_MESSAGE: &str = "The command has failed. Check permission or if command exist.";
+
 pub async fn execute(job: Job) -> JobResult {
     #[cfg(target_os = "windows")] {
         let mut cmd = Command::new("cmd");
@@ -12,12 +14,21 @@ pub async fn execute(job: Job) -> JobResult {
         cmd.args(&job.args);
         let output = cmd.output().await;
 
-        let out_unwrap = output.unwrap();
-
-        JobResult { 
-            exitcode: out_unwrap.status.code().unwrap_or(-1), 
-            stdout: String::from_utf8_lossy(&out_unwrap.stdout).to_string(), 
-            stderr: String::from_utf8_lossy(&out_unwrap.stderr).to_string()
+        match output {
+            Ok(o) => {
+                return JobResult { 
+                    exitcode: o.status.code().unwrap_or(-1), 
+                    stdout: String::from_utf8_lossy(&o.stdout).to_string(), 
+                    stderr: String::from_utf8_lossy(&o.stderr).to_string()
+                }
+            },
+            Err(_) => {
+                return JobResult {
+                    exitcode: 1, 
+                    stdout: String::from(""), 
+                    stderr: String::from(FAILED_MESSAGE)
+                }
+            }
         }
     }
 
@@ -28,12 +39,21 @@ pub async fn execute(job: Job) -> JobResult {
         cmd.args(&job.args);
         let output = cmd.output().await;
 
-        let out_unwrap = output.unwrap();
-
-        JobResult { 
-            exitcode: out_unwrap.status.code().unwrap_or(-1), 
-            stdout: String::from_utf8_lossy(&out_unwrap.stdout).to_string(), 
-            stderr: String::from_utf8_lossy(&out_unwrap.stderr).to_string()
+        match output {
+            Ok(o) => {
+                return JobResult { 
+                    exitcode: o.status.code().unwrap_or(-1), 
+                    stdout: String::from_utf8_lossy(&o.stdout).to_string(), 
+                    stderr: String::from_utf8_lossy(&o.stderr).to_string()
+                }
+            },
+            Err(_) => {
+                return JobResult {
+                    exitcode: 1, 
+                    stdout: String::from(""), 
+                    stderr: String::from(FAILED_MESSAGE)
+                }
+            }
         }
     }
     
