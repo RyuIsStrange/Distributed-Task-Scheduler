a6ed4eebcaa92c9bbc6b8ff1f94bd8340fb39574
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2026-04-20 13:45:22 -0500

    feat: create rest of metric values

--- crates/coordinator/src/metrics.rs
+++ crates/coordinator/src/metrics.rs
@@ -1,5 +1,5 @@
+use prometheus::{Counter, CounterVec, Gauge, GaugeVec, register_counter, register_counter_vec, register_gauge, register_gauge_vec};
 use std::sync::LazyLock;
-use prometheus::{Counter, CounterVec, register_counter, register_counter_vec};
 
 pub static JOBS_COMPLETED_TOTAL: LazyLock<Counter> = LazyLock::new(|| {
     register_counter!(
@@ -16,3 +16,31 @@ pub static JOBS_SUBMITTED_TOTAL: LazyLock<CounterVec> = LazyLock::new(|| {
     ).unwrap()
 });
 
+pub static JOBS_FAILED_TOTAL: LazyLock<Counter> = LazyLock::new(||{
+    register_counter!(
+        "jobs_failed_total",
+        "Total number of jobs that have failed"
+    ).unwrap()
+});
+
+pub static QUEUE_DEPTH: LazyLock<GaugeVec> = LazyLock::new(|| {
+    register_gauge_vec!(
+        "queue_depth",
+        "Current number of jobs pending broken down by priority",
+        &["priority"]
+    ).unwrap()
+});
+
+pub static ACTIVE_WORKERS: LazyLock<Gauge> = LazyLock::new(|| {
+    register_gauge!(
+        "active_workers",
+        "Total of alive workers"
+    ).unwrap()
+});
+
+pub static JOBS_WAITING_TOTAL: LazyLock<Gauge> = LazyLock::new(|| {
+    register_gauge!(
+        "jobs_waiting_total",
+        "Total number of jobs blocked on dependencies"
+    ).unwrap()
+});
